ce58d23bbe1d5e8e3fbd60a85f94a807887369e1
Roy Bebru <roybebru@gmail.com>
2024-01-16 12:58:09 +0200

    .DB_BOX is moved over repository folder

--- do.docker.postgres-vect.run-as-daemon.sh
+++ do.docker.postgres-vect.run-as-daemon.sh
@@ -7,7 +7,7 @@ DB_HOST=0.0.0.0
 DB_PORT=5432
 
 sudo docker run -p "$DB_HOST:$DB_PORT":5432 \
-    -v "$SCRIPT_PATH/.DB_BOX":/var/lib/postgresql/data \
+    -v "$SCRIPT_PATH/../.DB_BOX":/var/lib/postgresql/data \
     -e POSTGRES_PASSWORD="Gra.dum0k" \
     -e "TZ=Europe/Kiev" \
     --rm \
